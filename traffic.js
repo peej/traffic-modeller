33d1674329aa70efb94d1981a98019ad2320e41c
Paul James <paul@peej.co.uk>
2013-09-06 18:19:19 +0100

    General tidying

--- traffic.js
+++ traffic.js
@@ -1,9 +1,10 @@
 
 var Traffic = {
     tick: 1000,
-    vehicleLengthInPixels: 50, // length of one vehicle as drawn on the canvas
+    vehicleLengthInPixels: 10, // length of one vehicle as drawn on the canvas
     name: null,
     phase: 0,
+    verbose: false,
     extend: function (obj) {
         function F() {};
         F.prototype = this;
@@ -20,13 +21,15 @@ var Traffic = {
 
 Traffic.run = function (canvas, startNodes) {
     var context = canvas[0].getContext("2d");
+
     return window.setInterval(function () {
     //return window.setTimeout(function () {
         context.clearRect(0, 0, canvas.width(), canvas.height());
         Traffic.render.nodeRendered = [];
         $.each(startNodes, function () {
             Traffic.render(canvas, this);
-            if (Traffic.phase % this.frequency == 0) {
+            //if (Traffic.phase % this.frequency == 0) {
+            if (Math.random() > 1 / this.frequency) {
                 this.transferVehicle(Traffic.Vehicle.create({
                     name: "from " + this.name
                 })); // increment start node
@@ -47,11 +50,11 @@ Traffic.render = function (canvas, node) {
             if (Traffic.render.nodeRendered.indexOf(this) == -1) {
                 Traffic.render.nodeRendered.push(this);
                 this.render(context);
-                Traffic.render(canvas, this.endNode);
+                Traffic.render(canvas, this.end);
                 if (this.vehicles.length > 0) {
                     var veh = this.vehicles[0];
                     if (veh.exitPhase <= Traffic.phase) {
-                        if (this.endNode.transferVehicle(veh, this)) {
+                        if (this.end.transferVehicle(veh, this)) {
                             this.removeVehicle();
                         }
                     }
@@ -82,7 +85,7 @@ Traffic.Node = Traffic.extend({
         canvas.strokeStyle = this.color;
         canvas.fillStyle = this.color;
         canvas.lineWidth = 1;
-        canvas.arc(this.x, this.y, 5, 0, 360, false);
+        canvas.arc(this.x + 10, this.y + 10, 5, 0, 360, false);
         canvas.fill();
         canvas.stroke();
     },
@@ -104,7 +107,7 @@ Traffic.Node = Traffic.extend({
             typeof this.paths == "undefined" || this.paths.length == 0 || // there are no onward paths (is a dead end)
             this.busy // (is busy with another vehicle)
         ) {
-            console.log("Vehicle '" + veh.name + "' waiting at '" + this.name + "'");
+            if (Traffic.verbose) console.log("Vehicle '" + veh.name + "' waiting at '" + this.name + "'");
             this.busy = true;
             fromPath.color = "#f00";
             return this.transferBlocked(veh, fromPath);
@@ -128,7 +131,7 @@ Traffic.Node = Traffic.extend({
         this.busy = false;
         if (this.paths) {
             $.each(this.paths, function () {
-                this.endNode.reset();
+                this.end.reset();
             });
         }
     }
@@ -141,7 +144,6 @@ Traffic.StartNode = Traffic.Node.extend({
 
 Traffic.EndNode = Traffic.Node.extend({
     transferVehicle: function (veh, fromPath) { // exit nodes always have room
-        console.log("Vehicle '" + veh.name + "' exits from node '" + this.name + "'");
         return true;
     }
 });
@@ -174,21 +176,24 @@ Traffic.ControlledNode = Traffic.Node.extend({
         G.entryPaths = [];
         return G;
     },
+
+    transferAllowed: function (veh, fromPath) {
+        var pathNum = this.entryPaths.indexOf(fromPath);
+        return this.paths[pathNum].addVehicle(veh);
+    }
 });
 
 Traffic.Path = Traffic.extend({
     
-    startNode: null, // node that starts this path
-    endNode: null, // node that ends this path
+    name: "Un-named",
+    start: null, // node that starts this path
+    end: null, // node that ends this path
     color: "#00f",
 
-    create: function (startNode, endNode) {
-        var G = this.extend({
-            startNode: startNode,
-            endNode: endNode
-        });
-        startNode.addPath(G);
-        endNode.hasPath(G);
+    create: function (props) {
+        var G = this.extend(props);
+        G.start.addPath(G);
+        G.end.hasPath(G);
         G.vehicles = [];
         return G;
     },
@@ -201,15 +206,15 @@ Traffic.Path = Traffic.extend({
         canvas.beginPath();
         canvas.strokeStyle = this.color;
         canvas.lineWidth = (this.utilisation() / 10) + 1;
-        canvas.moveTo(this.startNode.x, this.startNode.y);
-        canvas.lineTo(this.endNode.x, this.endNode.y);
+        canvas.moveTo(this.start.x + 10, this.start.y + 10);
+        canvas.lineTo(this.end.x + 10, this.end.y + 10);
         canvas.stroke();
     },
 
     capacity: function () { // the maximum number of vehicle this path can hold
         //return 2; // should be based on the length of the path
-        var x = Math.abs(this.startNode.x - this.endNode.x);
-        var y = Math.abs(this.startNode.y - this.endNode.y);
+        var x = Math.abs(this.start.x - this.end.x);
+        var y = Math.abs(this.start.y - this.end.y);
         var h = Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / Traffic.vehicleLengthInPixels);
         this.capacity = function () {
             return h;
